025f8c71a2296ac381b8888ced20209971c825c3
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2019-10-08 03:45:34 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Util/Loader.php
+++ src/Util/Loader.php
@@ -96,9 +96,9 @@ class Loader
     protected function globFiles(string $group, string $environment): array
     {
         $files = [];
-        $grp = ($group ?: '*') . '.*';
+        $grp = ($group ?: '*').'.*';
         foreach ($this->searchDirs($environment) as $dir) {
-            $globs = \glob($dir . \DIRECTORY_SEPARATOR . $grp);
+            $globs = \glob($dir.\DIRECTORY_SEPARATOR.$grp);
             $files = array_merge($files, $globs ?: []);
         }
         return $files;
@@ -112,7 +112,7 @@ class Loader
      */
     protected function searchDirs(string $environment): array
     {
-        $envs  = preg_split(
+        $envs = preg_split(
             '~/~',
             trim($environment ?: $this->environment, '/'),
             -1,
@@ -120,9 +120,9 @@ class Loader
         );
 
         $d = $this->root_dir;
-        $dirs = [ $d ];
+        $dirs = [$d];
         foreach ($envs as $p) {
-            $d .= \DIRECTORY_SEPARATOR . $p;
+            $d .= \DIRECTORY_SEPARATOR.$p;
             $dirs[] = $d;
         }
         return $dirs;
